c1889b117ed5db776e9a4d48de2cdb0af0e4afe4
gehadshtewy <gehad.h1988@gmail.com>
2023-06-05 23:29:38 +0300

    050623

--- client/App.js
+++ client/App.js
@@ -2,11 +2,24 @@ import React,{useState, useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { TabsNavigator } from "./src/navigation/index.js";
+import { DefaultTheme, Provider } from 'react-native-paper';
+import AppColors from './src/components/AppColors.js';
+
+ const theme = {
+  ...DefaultTheme,
+  colors: {
+   ...DefaultTheme.colors,
+   secondaryContainer: AppColors.black
+  }
+ } 
+
 export default function App() {
   return (
-    <NavigationContainer>
+    <Provider theme={theme}>
+      <NavigationContainer>
        <TabsNavigator />
-    </NavigationContainer>
+     </NavigationContainer>
+    </Provider>
   );
 }
 
--- client/src/navigation/index.js
+++ client/src/navigation/index.js
@@ -1,6 +1,11 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import AppColors from "../components/AppColors";
 
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+
+
 import Shopping from "../screens/shopping/shopping";
 import Store from "../screens/store/store";
 import Favorites from "../screens/favorites/favorites";
@@ -9,9 +14,23 @@ const AppTabs = createMaterialBottomTabNavigator();
 export const TabsNavigator = () => {
     return(
         <AppTabs.Navigator activeColor={AppColors.green} inactiveColor={AppColors.green_90} barStyle={{backgroundColor: AppColors.black}}>
-            <AppTabs.Screen name="Store" component={Store} />
-            <AppTabs.Screen name="favorites" component={Favorites} />
-            <AppTabs.Screen name="Shopping" component={Shopping} />
+            <AppTabs.Screen name="Store" component={Store} 
+            options={{
+                tabBarLabel: 'Store', 
+                tabBarIcon:({color}) => (<MaterialCommunityIcons name="store-plus" color={color} size={30} />)
+            }} />
+
+            <AppTabs.Screen name="favorites" component={Favorites} 
+              options={{
+                tabBarLabel: 'Favorites', 
+                tabBarIcon:({color}) => (<MaterialIcons name="favorite" color={color} size={30} />)
+            }}  />
+
+            <AppTabs.Screen name="Shopping" component={Shopping} 
+            options={{
+                tabBarLabel: 'Shopping', 
+                tabBarIcon:({color}) => (<Fontisto name="shopping-basket-add" color={color} size={25} />)
+            }} /> 
         </AppTabs.Navigator>
     )
 }
